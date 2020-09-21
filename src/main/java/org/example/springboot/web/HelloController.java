db0b63853ed28e114607b67910d3b693be42c611
Minwook11 <alsdnr4874@gmail.com>
2020-09-21 18:58:26 +0900

    Chapter02 Complete - Jave Lombok 적용 - Spring에서의 Unit Test 경험

---
+++ src/main/java/org/example/springboot/web/HelloController.java
@@ -0,0 +1,18 @@
+package org.example.springboot.web;
+ import org.example.springboot.web.dto.HelloResponseDto;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RequestParam;
+ import org.springframework.web.bind.annotation.RestController;
+
+ @RestController
+public class HelloController {
+     @GetMapping("/hello")
+     public String hello() {
+         return "Hello";
+     }
+
+     @GetMapping("/hello/dto")
+     public HelloResponseDto hellodto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+         return new HelloResponseDto(name, amount);
+     }
+}
